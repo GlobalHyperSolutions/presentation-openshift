6fa6e6a71640b55cbb506bb9ad8913787af48067
Sezgin KEŞCİOĞLU <sezginkescioglu@outlook.com>
2022-11-28 22:32:12 +0300

    OC Client ile Spring Boot Uygulaması Deployment

--- Openshift 4 - OC Client ile Spring Boot Uygulaması Deployment/OC Komutları.sh
+++ Openshift 4 - OC Client ile Spring Boot Uygulaması Deployment/OC Komutları.sh
@@ -4,7 +4,7 @@ oc new-build --name=ui-edutr --image-stream=java:11 --binary=true -n hyper-appli
 # Bir BuildConfig üzerinden binary bir artifact ın container imajını oluşturma
 oc start-build ui-edutr --from-file=C:\LAB\TR-EDU-UI-0.0.1-SNAPSHOT.jar --follow
 
-# Bir BuildConfig üzerinden DeploymentConfig oluturmak ve bu DeploymentConfig üzerinden POD ayağa kaldırmak
+# Bir BuildConfig üzerinden DeploymentConfig oluşturmak ve bu DeploymentConfig üzerinden POD ayağa kaldırmak
 oc new-app ui-edutr:latest -l app=ui-edutr -n hyper-applications-edu-tr --as-deployment-config
 
 # Bir servisin router'ını oluşturmak
@@ -15,6 +15,9 @@ oc get pods
 
 # Bulunulan projedeki bir pod un yaml tanımını görme
 oc get pod/[POD_NAME] -o yaml
+
+# Belirli bir pod'un loglarını sürekli takip modunda görüntüleme
+oc logs pod/[POD_NAME] --follow
 
 # Bulunulan projedeki servis listesini (network) görme
 oc get svc
